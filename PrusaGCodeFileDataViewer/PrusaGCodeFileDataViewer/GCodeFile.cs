f1c460d6f26d0f7865dae7ddb2790b94150f81b9
Patrick <pnelson1@ycp.edu>
2021-05-06 19:54:05 -0400

    Added GCode file class

---
+++ PrusaGCodeFileDataViewer/PrusaGCodeFileDataViewer/GCodeFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrusaGCodeFileDataViewer
+{
+    public class GCodeFile
+    {
+        // File data
+        public string FileName { get; set; }
+        public int FileSize { get; set; }
+
+        // Specifications
+        public double FilamentSpoolCost { get; set; }
+        public double FilamentUsed { get; set; }
+        public double FilamentUsedCost { get; set; }
+
+        public GCodeFile() { }
+    }
+}
